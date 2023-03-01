79c446872a59cbbe893b73b896e37e7f5148858f
undarez <fortuna77320@gmail.com>
2023-03-01 18:07:41 +0100

    nouvelle modif avant soutenance

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const router = express.Router()
-const userCtrl = require('../controller/user');
+const userCtrl = require('../controller/User');
 
 // on utilie les post car le fron va envoyer les email et mot de pass
 router.post('/signup', userCtrl.signup);
